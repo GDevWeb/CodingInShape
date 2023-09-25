0e1cc5918bb41bd009efd8006b2cd05712126207
o_thollas <o_thollas@hotmail.com>
2023-09-25 09:04:24 +0200

    randomquote modif

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -5,40 +5,67 @@ import reload from './refresh_icon.png'
 
 
 const RandomQuote = () => {
-    
-    const api="https://type.fit/api/quotes";
-    let quotes = [];
 
-    async function loadQuotes(){
-        const response = await fetch(api);
-        quotes = await response.json();
-    }
-    
-    const [quote, setQuote] = useState({
-        text: "Tient toi droit.",
+
+    let quotes = [{
+        text: "Tiens toi droit.",
         author: "Maman",
+    },
+    {
+        "text": "Vas ranger ta chambre.",
+        "author": "Maman"
+    },
+    {
+        "text": "Mais qui m'a donné un enfant aussi bete",
+        "author": "Papa"
+    },
+    {
+        "text": "Reprend une part de gateau, tu es tout maigre",
+        "author": "Mami"
+    },
+    {
+        "text": "tiens prends ça, et ne dis rien à ta mami",
+        "author": "Papi"
+    },
+    {
+        "text": "De toute façon tu as triché",
+        "author": "petit Frere"
+    },
+    {
+        "text": "Tu auras le droit de jouer aprés moi",
+        "author": "Grand frere"
+    }];
+
+    // async function loadQuotes(){
+    //     const response = await fetch("https://type.fit/api/quotes");
+    //     quotes = await response.json();
+    // }
+
+    const [quote, setQuote] = useState({
+        text: "Tips pour toi ",
+        author: "Webmaster",
     });
 
     const random = () => {
-        const select = quotes[Math.floor(Math.random()*quotes.length)];
+        const select = quotes[Math.floor(Math.random() * quotes.length)];
         setQuote(select);
     }
-    
+
     return (
-    <div className='container'>
-        <div className="quote"> {quote.text}</div>
-        <div>
-            <div className="line"></div>
-            <div className="bottom">
-                <div className="author">{quote.author}</div>
-                <div className="icons">
-                    <img src={twitter} alt="" />
-                    <img src={reload} onClick={()=>{random()}} alt="" />
+        <div className='container'>
+            <div className="quote"> {quote.text}</div>
+            <div>
+                <div className="line"></div>
+                <div className="bottom">
+                    <div className="author">{quote.author}</div>
+                    <div className="icons">
+                        <img src={twitter} alt="" />
+                        <img src={reload} onClick={() => { random() }} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default RandomQuote
