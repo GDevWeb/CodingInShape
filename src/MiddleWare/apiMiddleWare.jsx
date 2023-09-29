09b63c89ffa8bb71b2cf64d4f4c9e031b67f4ce5
Gaëtan Dammaretz <113015001+Aescanor@users.noreply.github.com>
2023-09-29 17:31:21 +0200

    Update apiMiddleWare.jsx

--- src/MiddleWare/apiMiddleWare.jsx
+++ src/MiddleWare/apiMiddleWare.jsx
@@ -1,10 +1,10 @@
-import axios from 'axios'; // Vous devrez peut-être installer axios si ce n'est pas déjà fait
+import axios from 'axios'; 
 import { loginSuccess } from '../redux/Slices/authSlice';
 import store from '../redux/store';
 
 const apiMiddleware = (store) => (next) => async (action) => {
   if (action.type === loginSuccess.type) {
-    // Lorsque l'action loginSuccess est déclenchée, configurez l'en-tête de l'API avec le token
+    // Lorsque l'action loginSuccess est déclenchée, l'en-tête de l'API avec le token
     const token = action.payload;
 
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
